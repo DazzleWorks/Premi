7b6c0fbb9377f2a3802c6c36f698e8be9154ff12
valerioburlin <valerio.burlin@gmail.com>
2015-06-19 15:56:23 +0200

    aggiunto a readme istruzione per commenti

--- app/Model/Slide.php
+++ app/Model/Slide.php
@@ -1,18 +1,13 @@
-ISTRUZIONI PER L'USO:
+<?php
+namespace Premi;
 
-1. rinominare file '.env.example' --> .env
-2. modificare DB_HOST con il proprio localhost (verificarlo con mongodb)
-3. aggiungere cartella /vendor
-4. aggiungere cartella /node_modules
-
-ISTTRUZIONI COMMENTI:
-form standard che deve avere ogni file:
+use Jenssegers\Mongodb\Model as Eloquent;
 
 /**
  * @file: slide.php
  * @author: Burlin Valerio
  * @date: 2015-06-19
- * @description: This class implements....
+ * @description: This class implements
  * 
  * +---------+------+------------+--------+-------------+ 
  * | Version | Date | Programmer | Modify | Description |
@@ -20,5 +15,6 @@ form standard che deve avere ogni file:
  * |         |      |            |        |             |
  * +---------+------+------------+--------+-------------+
  */
-
-Lingua RIGOROSAMENTE INGLESE
+class Slide extends Eloquent {
+    
+}
