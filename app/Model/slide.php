6d97af50fb3f718125b38224dd4cc65da0b8726e
valerioburlin <valerio.burlin@gmail.com>
2015-06-19 16:55:12 +0200

    aggiunta classe slide.php

--- app/Model/slide.php
+++ app/Model/slide.php
@@ -5,16 +5,32 @@ use Jenssegers\Mongodb\Model as Eloquent;
 
 /**
  * @file: slide.php
- * @author: Burlin Valerio
+ * @author: DazzleWorks
  * @date: 2015-06-19
- * @description: This class implements
+ * @description: This class stores slide data that is retrieved by the slide 
+ *               controller
  * 
- * +---------+------+------------+--------+-------------+ 
- * | Version | Date | Programmer | Modify | Description |
- * +---------+------+------------+--------+-------------+
- * |         |      |            |        |             |
- * +---------+------+------------+--------+-------------+
+ * +---------+------------+----------------+--------------+--------------+ 
+ * | Version |    Date    |   Programmer   |    Modify    | Description  |
+ * +---------+------------+----------------+--------------+--------------+
+ * |  0.0.1  | 2015-06-19 | Burlin Valerio | class Slide  | create class |
+ * +---------+------------+----------------+--------------+--------------+
  */
 class Slide extends Eloquent {
     
+    /**
+     * Indicates if the model should be timestamped
+     * 
+     * @var timestamps bool
+     */
+    public $timestamps = false;
+    
+    /**
+     * The attributes of the class.
+     *
+     * @var x ->
+     * @var y ->
+     * @var components -> 
+     */
+    private $x, $y, $components = array();
 }
