85cb5dae8691298d8679253a984a355d98e89175
Nicola Carraro <nicola.carraro.18@gmail.com>
2015-08-04 15:16:36 +0200

    aggiunta view reset password

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,24 +1,32 @@
 <!-- resources/views/auth/reset.blade.php -->
 
-<form method="POST" action="/password/reset">
-    {!! csrf_field() !!}
-    <input type="hidden" name="token" value="{{ $token }}">
+<div>
+    <h1>Premi - Reset password</h1>
+</div>
 
-    <div>
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
+<div>
+    <form method="POST" action="/password/reset">
+        <input type="hidden" name="token" value="{{ $token }}">
 
-    <div>
-        <input type="password" name="password">
-    </div>
+        <div>
+            <label>Email: </label>
+            <input type="email" name="email" value="{{ old('email') }}">
+        </div>
 
-    <div>
-        <input type="password" name="password_confirmation">
-    </div>
+        <div>
+            <label>New password: </label>
+            <input type="password" name="password">
+        </div>
 
-    <div>
-        <button type="submit">
-            Reset Password
-        </button>
-    </div>
-</form>
+        <div>
+            <label>Repeat password: </label>
+            <input type="password" name="password_confirmation">
+        </div>
+
+        <div>
+            <button type="submit">
+                Reset Password
+            </button>
+        </div>
+    </form>
+</div>
