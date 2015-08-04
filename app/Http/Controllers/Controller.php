ed9eaa4c2228a498bf010b413374541de0c8e939
valerioburlin <valerio.burlin@gmail.com>
2015-08-04 14:48:25 +0200

    aggiunti metodi per controllo validazione registrazione

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,19 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
+/**
+ * @file: app/Http/Controller/Controller.php
+ * @author: DazzleWorks
+ * @date: 2015-07-15
+ * @description: This abstract class is the basis of other controller.
+ *
+ * +---------+------------+----------------------------------+-----------------+
+ * | Version |     Date   |  Programmer   |     Modify       |  Description    |
+ * +---------+------------+---------------+------------------+-----------------+
+ * |  1.0.0  | 2015-06-20 |Burlin Valerio | class Controller | create the      |
+ * |         |            |               |                  | abstract class  |
+ * +---------+------------+---------------+------------------+-----------------+
+ */
 abstract class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
