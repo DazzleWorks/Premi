c7bbbffed167bfd0454792b5e4e332ef85b642bc
Bogdan Suierica <bogdansuierica@yahoo.com>
2015-08-11 18:47:55 +0200

    aggiunto controllo email già presente nel database

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -47,7 +47,7 @@ class AuthController extends Controller
     {
         $rules = [
             'username' => 'required|max:255|unique:users',
-            'email' => 'required|email|max:255',
+            'email' => 'required|email|max:255|unique:users',
             'firstName' => 'required|max:255|alpha',
             'lastName' => 'required|max:255|alpha',
             'password' => 'required|confirmed|min:8',
