d6ed192ea31c176ef64dbaa75549cb683dd98399
Nicola Carraro <nicola.carraro.18@gmail.com>
2015-08-18 15:20:34 +0200

    corretto ritorno parametri UserController backend

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,7 @@
 
 namespace Premi\Http\Controllers;
 
-use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Premi\Http\Controllers\Controller;
 use Premi\Model\User;
 
@@ -10,13 +10,13 @@ use Premi\Model\User;
  * @file: app/Http/Controller/UserController.php
  * @author: DazzleWorks
  * @date: 2015-06-20
- * @description: This class handles the deleting and viewing, through a specific 
+ * @description: This class handles the deleting and viewing, through a specific
  * view, of a user.
  *
  * +---------+------------+---------------+----------------------+-------------+
  * | Version |     Date   |  Programmer   |        Modify        | Description |
  * +---------+------------+---------------+----------------------+-------------+
- * |  1.0.0  | 2015-06-20 |Burlin Valerio | class UserController |create class,| 
+ * |  1.0.0  | 2015-06-20 |Burlin Valerio | class UserController |create class,|
  * |         |            |               |                      |and its rest |
  * |         |            |               |                      |functions    |
  * +---------+------------+---------------+----------------------+-------------+
@@ -33,12 +33,12 @@ class UserController extends Controller
         if(!\Auth::user()) {
             return response()->json(['status' => 'utente non loggato']);
         }
-      
-        $data = \Auth::user()->groupBy()->get(['_id','email','firstName','lastName','username']);
-        
+
+        $data = User::where('username', $username)->get(['_id','email','firstName','lastName','username']);
+
         return response()->json($data);
     }
-    
+
     /**
      * Update the specified resource in storage.
      * @param Illuminate\Http\Request
@@ -48,17 +48,17 @@ class UserController extends Controller
     public function update(Request $request,$username)
     {
         $user = \Auth::user();
-        
+
         $user->email = $request->get('email');
         $user->firstName = $request->get('firstName');
         $user->secondName = $request->get('secondName');
         $user->password = $request->get('password');
-        
+
         $user->save();
-        
+
         return response()->json(['status' => true]);;
     }
-    
+
     /**
      * Remove the specified resource from storage.
      * @param String $username: the username of a user
@@ -67,9 +67,9 @@ class UserController extends Controller
     public function destroy($username)
     {
         $user = \Auth::user();
-                
+
         $user->delete();
-        
+
         return response()->json(['status' => true]);;
     }
 }
