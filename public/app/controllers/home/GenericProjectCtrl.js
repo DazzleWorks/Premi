403c6c89287e8b29f1310ee9db6a9ae289c27f66
fabioros <fabio.ros90@gmail.com>
2015-08-20 00:29:36 +0200

    SVG first slide preview on user search added

--- public/app/controllers/home/GenericProjectCtrl.js
+++ public/app/controllers/home/GenericProjectCtrl.js
@@ -2,7 +2,7 @@
 
 angular.module('app.controllers.GenericProjectCtrl', ['ngRoute'])
 
-    .controller('GenericProjectCtrl', ['$scope', '$rootScope', '$modal', function($scope, $rootScope, $modal) {
+    .controller('GenericProjectCtrl', ['$scope', '$rootScope', '$modal', '$sce', '$window', '$document', function($scope, $rootScope, $modal, $sce, $window, $document) {
         console.log($scope.userOfInterest.projects);
 
         $rootScope.currentGenericProject = {
@@ -17,14 +17,41 @@ angular.module('app.controllers.GenericProjectCtrl', ['ngRoute'])
             // maxY: 0
         };
 
+        $scope.adjustSVGViewbox = function(svgString){    //da chiamare anche al resize della pagina
+            //1 calcolo dimensione finestra
 
+            //2 trovo larghezza in pixel ed altezza = larghezza*3/4 del ViewBox
+
+
+            //3 se esiste l'attributo  __ viewBox="0 0 800 600" __ aggiorno le dimensioni, altrimenti lo aggiungo
+            var SVGWidth=document.getElementById('presentationFrontSvgContainer').clientWidth;
+            var indexBeginWidth = svgString.lastIndexOf("width=\"")+7;
+            var indexEndWidth=    svgString.indexOf(" height")-2;
+            var indexBeginHeight = svgString.lastIndexOf("height=\"")+8;
+            var indexEndHeight=    svgString.indexOf("\" xml:space")-1;
+            $scope.originalWidth=svgString.substring(indexBeginWidth,indexEndWidth);
+            $scope.originalHeight=svgString.substring(indexBeginHeight,indexEndHeight);
+            console.log($scope.originalHeight);
+            svgString = svgString.replace("svg","svg viewBox='0 0 "+ $scope.originalWidth +" "+ $scope.originalHeight +" ' width='100%' height='"+ (SVGWidth * 3/4) +"' class='svg-content beige' ");
+
+            /*
+            *   PEZZO DA AGGIUNGERE
+
+              width="156px" height="117.064px"
+              viewBox="0 0 156 117.064"
+              preserveAspectRatio="xMidYMid"
+
+
+            * */
+
+             return svgString;
+        };
 
          $scope.findProjectById = function(id){
-             console.log($scope.userOfInterest.projects);
+             //console.log($scope.userOfInterest.projects);
             var k;
             var obj = {id: "", name: "", presentation: "",firstSlide: "", theme:"sky", transition:"slide"};
             for (k = 0; k < $scope.userOfInterest.projects.length; ++k) {
-                console.log($scope.userOfInterest.projects[k]._id.$id);
                 if($scope.userOfInterest.projects[k]._id.$id === id) {
 
                     obj.id=$scope.userOfInterest.projects[k]._id.$id;
@@ -45,8 +72,13 @@ angular.module('app.controllers.GenericProjectCtrl', ['ngRoute'])
                         obj.transition="slide";
 
                     //svg
-                    if($scope.userOfInterest.projects[k].svg !== undefined)
-                        obj.svg=$scope.userOfInterest.projects[k].svg;
+                    //console.log(obj.svg=$scope.userOfInterest.projects[k].presentation.slides[0].svg);
+
+                    var svg= $scope.userOfInterest.projects[k].presentation.slides[0].svg;
+                    if(svg !== undefined && svg !== "" && svg !== null) {
+                        obj.svg = $sce.trustAsHtml($scope.adjustSVGViewbox($scope.userOfInterest.projects[k].presentation.slides[0].svg));
+                        $scope.apply;
+                    }
                     else
                         obj.svg="";
                 }
@@ -63,7 +95,7 @@ angular.module('app.controllers.GenericProjectCtrl', ['ngRoute'])
             $rootScope.currentGenericProject.firstSlide = obj.firstSlide;
             $rootScope.currentGenericProject.theme = obj.theme;
             $rootScope.currentGenericProject.transition = obj.transition;
-
-            console.log($rootScope.currentGenericProject);
+            $rootScope.currentGenericProject.svg = obj.svg;
+           // console.log($rootScope.currentGenericProject);
         };
     }]);
--- public/app/directives/home/genericProject.js
+++ public/app/directives/home/genericProject.js
@@ -2,6 +2,7 @@ angular.module('app.directives.genericProject', ['ngRoute'])
     .directive('genericProject', [function() {
         var directive = {};
         directive.restrict = 'E';
+        directive.transclude= true;
         directive.templateUrl = "app/templates/genericProject.html";
         return directive;
 }]);
